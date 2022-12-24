d8d5c30b24c4d484c85524b3a012ab5f541cdbdc
Levani49 <levani.pachuashvili.1@btu.edu.ge>
2022-12-25 01:11:02 +0400

    feat: logic is ready and questions are added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
@@ -6,16 +7,21 @@ const answerButtonsElement = document.getElementById("answer-buttons");
 let shuffleQuestions, currentQuestionIndex;
 
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
   startButton.classList.add("hide");
-  shuffleQuestions = question.sort(() => Math.random() - 0.5);
+  shuffleQuestions = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
   questionContainerElement.classList.remove("hide");
   setNextQuestion();
 }
 
 function setNextQuestion() {
+  resetState();
   showQuestion(shuffleQuestions[currentQuestionIndex]);
 }
 
@@ -33,16 +39,78 @@ function showQuestion(question) {
   });
 }
 
-function selectAnswer(e) {}
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
 
-const question = [
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffleQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+const questions = [
   {
-    question: "what is 2 + 2",
-    answer: [
+    question: "What is 2 - 2?",
+    answers: [
+      { text: "0", correct: true },
+      { text: "-2", correct: false },
+      { text: "4", correct: false },
+      { text: "5", correct: false },
+    ],
+  },
+  {
+    question: "what is 10 + 2?",
+    answers: [
+      { text: "12", correct: true },
+      { text: "11", correct: false },
+      { text: "10", correct: false },
+      { text: "9", correct: false },
+    ],
+  },
+  {
+    question: "what is 20 ÷ 2?",
+    answers: [
+      { text: "11", correct: false },
+      { text: "2", correct: false },
+      { text: "10", correct: true },
+      { text: "40", correct: false },
+    ],
+  },
+  {
+    question: "What is 2 + 2?",
+    answers: [
+      { text: "3", correct: false },
+      { text: "2", correct: true },
       { text: "4", correct: true },
-      { text: "22", correct: false },
-      { text: "4", correct: false },
-      { text: "4", correct: false },
+      { text: "0", correct: true },
     ],
   },
 ];
